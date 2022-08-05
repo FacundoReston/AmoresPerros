c19cc163e917b51921591aae773171f248086c61
Facundo Reston <facundoreston@hotmail.com>
2022-08-05 14:57:20 -0300

    cambios-5/8

--- php/form.php
+++ php/form.php
@@ -1,9 +1,15 @@
 <?php
 
 $pregunta = $_POST ['mensaje'];
+$excelente = $_POST ['atencion'];
+$buena = $_POST ['atencion'];
+$regular = $_POST ['atencion'];
+$mala = $_POST ['atencion'];
+$pesima = $_POST ['atencion'];
 
 $pregunta .= "Mensaje: " . $_POST ['mensaje']
 $pregunta .= "Enviado el " . date('d/m/Y', time());
+$mensaje = "Valoración de atención al cliente: " . $_POST ['atencion']
 
 $para = 'guzmanmariabelen@hotmail.com'
 $asunto = 'Este mail fue enviado desde la web AmoresPerros'
